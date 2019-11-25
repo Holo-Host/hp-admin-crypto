9dac843c9640f5be51eb6e5f42f0c36ddd172dc7
PJ <pj@imagine-nyc.com>
2019-11-25 10:14:10 +0100

    set up environment

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import ./pkgs.nix {} }:
+
+with pkgs;
+
+let
+  inherit (rust.packages.nightly) rustPlatform;
+  inherit (darwin.apple_sdk.frameworks) CoreServices Security;
+in
+
+{
+  hp-admin-authorize = buildRustPackage rustPlatform {
+    name = "hp-admin-authorize";
+    src = gitignoreSource ./.;
+    cargoDir = "server";
+
+    buildInputs = lib.optionals stdenv.isDarwin [ Security ];
+
+    doCheck = false;
+  };
+
+}

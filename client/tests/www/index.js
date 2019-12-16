e93e7952be6248a11d94ba9425310321fe43c22a
PJ <pj@imagine-nyc.com>
2019-12-16 13:00:24 +0100

    client: fix tests/www/

---
+++ client/tests/www/index.js
@@ -0,0 +1,15 @@
+import {HpAdminKeypair} from "hp-admin-crypto-client";
+
+const HC_PUBLIC_KEY = "3llrdmlase6xwo9drzs6qpze40hgaucyf7g8xpjze6dz32s957";
+const EMAIL= "pj@abba.pl";
+const PASSWORD= "abba";
+
+let kp = new HpAdminKeypair(HC_PUBLIC_KEY, EMAIL, PASSWORD);
+
+const payload = {
+    method: "get",
+    uri: "/someuri",
+    body_string: ""
+}
+
+console.log(kp.sign(payload));

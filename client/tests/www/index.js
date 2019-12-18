7d2e9e7a03867927b2adae9597417c2147189f65
PJ <pj@imagine-nyc.com>
2019-12-18 13:38:10 +0100

    update client/tests/www

--- client/tests/www/index.js
+++ client/tests/www/index.js
@@ -1,4 +1,4 @@
-import { HpAdminKeypair } from "../../pkg/hp_admin_key_manager";
+import { HpAdminKeypair } from "@holo-host/hp_admin_keypair";
 
 const HC_PUBLIC_KEY = "3llrdmlase6xwo9drzs6qpze40hgaucyf7g8xpjze6dz32s957";
 const EMAIL = "pj@abba.pl";

b86167c480d431691c9a892e3747d22133856751
PJ <pj@imagine-nyc.com>
2019-12-23 20:24:21 +0100

    server: extract_signature from header or query param

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,6 +6,7 @@ use futures::{
 };
 use hyper::header::{HeaderMap, HeaderName, HeaderValue};
 use hyper::{service, Body, Request, Response, Server, StatusCode};
+use url::form_urlencoded;
 
 use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
@@ -84,7 +85,15 @@ fn create_response(req: Request<Body>) -> impl Future<Item = Response<Body>, Err
                     }
                 };
 
-                let is_verified = match verify_request(payload, parts.headers, public_key) {
+                let signature = match extract_signature(parts.headers, parts.uri.query()) {
+                    Ok(s) => s,
+                    Err(e) => {
+                        debug!("Error while extracting signature: {}", e);
+                        return respond_success(false);
+                    }
+                };
+
+                let is_verified = match verify_request(payload, signature, public_key) {
                     Ok(b) => b,
                     Err(e) => {
                         debug!("Error while verifying signature: {}", e);
@@ -104,21 +113,40 @@ fn create_response(req: Request<Body>) -> impl Future<Item = Response<Body>, Err
     }
 }
 
+fn extract_signature(
+    headers: HeaderMap<HeaderValue>,
+    query_opt: Option<&str>,
+) -> Result<String, Box<dyn Error>> {
+    if let Some(signature) = headers.get(&*X_HPOS_ADMIN_SIGNATURE) {
+        return Ok(signature.to_str()?.to_string());
+    }
+
+    if let Some(query_str) = query_opt {
+        let args = form_urlencoded::parse(query_str.as_bytes()).into_owned();
+
+        for arg in args {
+            let (a, b) = arg;
+            if a == "X-Holo-Admin-Signature".to_string() {
+                return Ok(b);
+            }
+        }
+    }
+
+    Err("Can't read signature from Headers or query string X-Holo-Admin-Signature")?
+}
+
 fn verify_request(
     payload: Payload,
-    headers: HeaderMap<HeaderValue>,
+    signature: String,
     public_key: PublicKey,
 ) -> Result<bool, Box<dyn Error>> {
     let payload_vec = serde_json::to_vec(&payload)?;
 
-    if let Some(signature_base64) = headers.get(&*X_HPOS_ADMIN_SIGNATURE) {
-        if let Ok(signature_vec) = base64::decode_config(&signature_base64, base64::STANDARD_NO_PAD)
-        {
-            if let Ok(signature_bytes) = Signature::from_bytes(&signature_vec) {
-                if public_key.verify(&payload_vec, &signature_bytes).is_ok() {
-                    debug!("Signature verified successfully");
-                    return Ok(true);
-                }
+    if let Ok(signature_vec) = base64::decode_config(&signature, base64::STANDARD_NO_PAD) {
+        if let Ok(signature_bytes) = Signature::from_bytes(&signature_vec) {
+            if public_key.verify(&payload_vec, &signature_bytes).is_ok() {
+                debug!("Signature verified successfully");
+                return Ok(true);
             }
         }
     }
@@ -208,15 +236,16 @@ mod tests {
     use ed25519_dalek::{self, SECRET_KEY_LENGTH};
     use std::convert::From;
 
+    static EXPECTED_SIGNATURE: &str =
+        "EHl16e8ZRMhVk1BpvPxuc8PDCNUcZfWPDgU+GuOVX5r2SNzzwSK4WAXC8+Hc0lF2JpDcxMTEHVCp3KNAd4zlAA";
+    static SECRET: [u8; 32] = [
+        82, 253, 185, 87, 98, 217, 46, 233, 252, 159, 103, 182, 121, 229, 22, 25, 34, 216, 81, 60,
+        31, 204, 200, 63, 63, 233, 220, 47, 221, 74, 86, 129,
+    ];
+
     #[test]
     fn verify_signature_match_client() {
-        let expected_signature: &str =
-            "EHl16e8ZRMhVk1BpvPxuc8PDCNUcZfWPDgU+GuOVX5r2SNzzwSK4WAXC8+Hc0lF2JpDcxMTEHVCp3KNAd4zlAA";
-        let secret: [u8; 32] = [
-            82, 253, 185, 87, 98, 217, 46, 233, 252, 159, 103, 182, 121, 229, 22, 25, 34, 216, 81,
-            60, 31, 204, 200, 63, 63, 233, 220, 47, 221, 74, 86, 129,
-        ];
-        let secret_key = ed25519_dalek::SecretKey::from_bytes(&secret).unwrap();
+        let secret_key = ed25519_dalek::SecretKey::from_bytes(&SECRET).unwrap();
         let public_key = ed25519_dalek::PublicKey::from(&secret_key);
         let secret_key_exp = ed25519_dalek::ExpandedSecretKey::from(&secret_key);
 
@@ -235,36 +264,61 @@ mod tests {
             &mut signature_base64,
         );
 
-        assert_eq!(signature_base64, expected_signature);
+        assert_eq!(signature_base64, EXPECTED_SIGNATURE);
     }
 
     #[test]
-    fn verify_request_smoke() {
+    fn verify_request_from_header() {
         // Get a legit request_hash signature, agent_id
-        let secret: [u8; 32] = [0_u8; SECRET_KEY_LENGTH];
-        let secret_key = ed25519_dalek::SecretKey::from_bytes(&secret).unwrap();
+        let secret_key = ed25519_dalek::SecretKey::from_bytes(&SECRET).unwrap();
         let public_key = ed25519_dalek::PublicKey::from(&secret_key);
-        let secret_key_exp = ed25519_dalek::ExpandedSecretKey::from(&secret_key);
 
         // Now lets sign some payload
         let payload = Payload {
             method: "get".to_string(),
             request: "/someuri".to_string(),
-            body: Some("\"a:\"b\"\"".to_string()),
+            body: None,
         };
 
-        let signature = secret_key_exp.sign(&serde_json::to_vec(&payload).unwrap(), &public_key);
-        let mut signature_base64 = String::new();
-        base64::encode_config_buf(
-            signature.to_bytes().as_ref(),
-            base64::STANDARD_NO_PAD,
-            &mut signature_base64,
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "x-hpos-admin-signature",
+            EXPECTED_SIGNATURE.parse().unwrap(),
         );
 
-        let mut headers = HeaderMap::new();
-        headers.insert("x-hpos-admin-signature", signature_base64.parse().unwrap());
+        let signature = extract_signature(headers, None).unwrap();
 
-        assert_eq!(verify_request(payload, headers, public_key).unwrap(), true)
+        assert_eq!(
+            verify_request(payload, signature, public_key).unwrap(),
+            true
+        )
+    }
+
+    #[test]
+    fn verify_request_from_query() {
+        // Get a legit request_hash signature, agent_id
+        let secret_key = ed25519_dalek::SecretKey::from_bytes(&SECRET).unwrap();
+        let public_key = ed25519_dalek::PublicKey::from(&secret_key);
+
+        // Now lets sign some payload
+        let payload = Payload {
+            method: "get".to_string(),
+            request: "/someuri".to_string(),
+            body: None,
+        };
+
+        let headers = HeaderMap::new();
+        let encoded: String = form_urlencoded::Serializer::new(String::new())
+            .append_pair("foo", "bar")
+            .append_pair("X-Holo-Admin-Signature", EXPECTED_SIGNATURE)
+            .finish();
+
+        let signature = extract_signature(headers, Some(&encoded)).unwrap();
+
+        assert_eq!(
+            verify_request(payload, signature, public_key).unwrap(),
+            true
+        )
     }
 
     #[test]
@@ -277,12 +331,17 @@ mod tests {
         let payload = Payload {
             method: "get".to_string(),
             request: "/someuri".to_string(),
-            body: None
+            body: None,
         };
 
         let mut headers = HeaderMap::new();
         headers.insert("x-hpos-admin-signature", "Wrong signature".parse().unwrap());
 
-        assert_eq!(verify_request(payload, headers, public_key).unwrap(), false)
+        let signature = extract_signature(headers, None).unwrap();
+
+        assert_eq!(
+            verify_request(payload, signature, public_key).unwrap(),
+            false
+        )
     }
 }

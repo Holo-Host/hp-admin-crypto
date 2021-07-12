95ae0faecd6ceb10f09e4295c2d73e97233833cc
zo-el <joelulahanna@gmail.com>
2021-07-12 16:12:41 +0530

    updated build script for new wasm-pack

--- client/build.sh
+++ client/build.sh
@@ -7,9 +7,6 @@ PACKAGE_NAME=$(basename $(jq -r .name pkg/package.json | tr - _))
 
 cp pkg-nodejs/${PACKAGE_NAME}.js pkg/${PACKAGE_NAME}_node.js
 
-sed "s/${PACKAGE_NAME}.js')/${PACKAGE_NAME}_node.js')/" \
-  pkg-nodejs/${PACKAGE_NAME}_bg.js > pkg/${PACKAGE_NAME}_bg.js
-
 PACKAGE_JQ_FILTER=$(cat <<END
 .files += ["${PACKAGE_NAME}_bg.js"] | .main = "${PACKAGE_NAME}_node.js"
 END

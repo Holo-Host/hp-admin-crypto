a557cc5a0f256216a27d1277334fc9882af2ba25
zo-el <joelulahanna@gmail.com>
2021-12-22 22:34:23 +0530

    bump version

--- client/tests/www/index.js
+++ client/tests/www/index.js
@@ -1,5 +1,7 @@
 import { HpAdminKeypair } from "@holo-host/hp-admin-keypair";
 
+// const HpAdminKeypair = require('@holo-host/hp-admin-keypair').HpAdminKeypair
+
 const HC_PUBLIC_KEY = "5m5srup6m3b2iilrsqmxu6ydp8p8cr0rdbh4wamupk3s4sxqr5";
 const EMAIL = "pj@abba.pl";
 const PASSWORD = "abbaabba";
